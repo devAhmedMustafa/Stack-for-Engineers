d4446b837f6a3de86cd27b9be7d72a44adc47080
Ahmed Mustafa <am1804067@gmail.com>
2023-04-14 23:52:24 +0200

    login required

--- project/settings.py
+++ project/settings.py
@@ -28,13 +28,6 @@ REST_FRAMEWORK = {
 INSTALLED_APPS = [
 
     'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.facebook',
-    'allauth.socialaccount.providers.microsoft',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -60,8 +53,6 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
-
-SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -109,14 +100,6 @@ DATABASES = {
         'PORT':'6918',
     }
 }
-
-AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
 
 
 # Password validation
